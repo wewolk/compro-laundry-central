fbf97d313670e1e4d9d070c878c481b2518ebac8
NaufalAnantaSE <anantanaufal250@gmail.com>
2026-08-03 18:17:38 +0700

    fix metadata seo

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,31 +3,31 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.centrallaundyexpress.com',
+      url: 'https://www.centrallaundryexpress.com',
       lastModified: new Date(),
       changeFrequency: 'hourly',
       priority: 1,
     },
     {
-      url: 'https://www.centrallaundyexpress.com/galeri',
+      url: 'https://www.centrallaundryexpress.com/galeri',
       lastModified: new Date(),
       changeFrequency: 'hourly',
       priority: 0.8,
     },
         {
-      url: 'https://www.centrallaundyexpress.com/kontak',
+      url: 'https://www.centrallaundryexpress.com/kontak',
       lastModified: new Date(),
       changeFrequency: 'hourly',
       priority: 0.8,
     },
     {
-      url: 'https://www.centrallaundyexpress.com/layanan',
+      url: 'https://www.centrallaundryexpress.com/layanan',
       lastModified: new Date(),
       changeFrequency: 'hourly',
       priority: 0.8,
     },
     {
-      url: 'https://www.centrallaundyexpress.com/tentang',
+      url: 'https://www.centrallaundryexpress.com/tentang',
       lastModified: new Date(),
       changeFrequency: 'hourly',
       priority: 0.8,
